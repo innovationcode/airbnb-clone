b715e1c016d4e3df48bd1e51a8684dad94943057
lajawanti <lajawanti.dhake@gmail.com>
2020-08-27 23:11:30 -0400

    moving to home component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Header from './components/header/Header.js'; 
+import Home from './components/home/Home.js';
 
 import './App.css';
 
@@ -7,6 +8,10 @@ function App() {
   return (
     <div className="app">
         <Header />
+
+        {/* Banner */}
+        <Home />
+
     </div>
   );
 }
---
+++ src/components/home/Home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Banner from '../banner/Banner.js'
+
+import './Home.css';
+
+const Home = () => {
+    return (
+        <div className = 'home'>
+            <h2>Home component</h2>
+            <Banner />
+
+        </div>
+    )
+}
+
+export default Home;
